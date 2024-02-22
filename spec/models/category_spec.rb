915bf4d6d14e8483e71faa0c95dd4179d94e9111
beatrizChagas <beatriznery.12@gmail.com>
2024-02-22 18:28:30 -0300

    style: Remove whitespaces

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Category, type: :model do
     it { should have_many(:books) }
   end
 
-  describe 'validations' do 
+  describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_least(3) }
   end
